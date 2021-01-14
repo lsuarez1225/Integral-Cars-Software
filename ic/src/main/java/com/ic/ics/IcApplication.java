3c81ede51314bd2bfc1c4c351c042e725f866ac6
Luis Suarez <lsuarez1225@gmail.com>
2021-01-14 18:34:13 -0500

    Commit inicial con eclipse

---
+++ ic/src/main/java/com/ic/ics/IcApplication.java
@@ -0,0 +1,13 @@
+package com.ic.ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcApplication.class, args);
+	}
+
+}
